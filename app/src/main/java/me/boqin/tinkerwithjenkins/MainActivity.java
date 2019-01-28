7bebea4723376adb1cc8c20729a930bfbeb0ca57
vito <boqin2x@163.com>
2019-01-28 15:15:04 +0800

    添加flutter

--- app/src/main/java/me/boqin/tinkerwithjenkins/MainActivity.java
+++ app/src/main/java/me/boqin/tinkerwithjenkins/MainActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import io.flutter.facade.Flutter;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
@@ -13,23 +15,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.goto_qts).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                RetrofitManager.getInstance().getService().homeTopPage()
-//                        .subscribeOn(Schedulers.io())
-//                        .subscribe(new Consumer<Object>() {
-//                            @Override
-//                            public void accept(Object o) throws Exception {
-//
-//                            }
-//                        });
-                  Intent intent = new Intent(Intent.ACTION_VIEW);
-                  intent.setData(Uri.parse("zmxy://com.qts.customer/main/loading"));
-                  startActivity(intent);
-            }
-        });
+//        setContentView(R.layout.activity_main);
+        View flutterView = Flutter.createView(
+                MainActivity.this,
+                getLifecycle(),
+                "route1"
+        );
+        FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600, 800);
+        layout.leftMargin = 100;
+        layout.topMargin = 200;
+        setContentView(flutterView, layout);
+
+//        findViewById(R.id.goto_qts).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                RetrofitManager.getInstance().getService().homeTopPage()
+////                        .subscribeOn(Schedulers.io())
+////                        .subscribe(new Consumer<Object>() {
+////                            @Override
+////                            public void accept(Object o) throws Exception {
+////
+////                            }
+////                        });
+////                  Intent intent = new Intent(Intent.ACTION_VIEW);
+////                  intent.setData(Uri.parse("zmxy://com.qts.customer/main/loading"));
+////                  startActivity(intent);
+//                View flutterView = Flutter.createView(
+//                        MainActivity.this,
+//                        getLifecycle(),
+//                        "route1"
+//                );
+//                FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600, 800);
+//                layout.leftMargin = 100;
+//                layout.topMargin = 200;
+//                setContentView(flutterView, layout);
+//            }
+//        });
 //        Intent intent = new Intent(Intent.ACTION_VIEW);
 //        intent.setData(Uri.parse("qrouter://com.qts.customer/main/loading"));
 //        startActivity(intent);
